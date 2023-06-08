7725816fa5e5cf0bb6a963a4dbfd025d537e1eb7
Maksimkakir <125463514+Maksimkakir@users.noreply.github.com>
2023-06-09 00:47:09 +0300

    добавил измененное дз

--- project.py
+++ project.py
@@ -1,27 +1,45 @@
 import os
 import shutil
 
-root_folder_path = "путь к проекту 3"
+# Путь к папке А
+folder_A = 'путь к папке а'
 
-def sort_images(root_folder_path):
-    for folder_name in os.listdir(root_folder_path):
-        folder_path = os.path.join(root_folder_path, folder_name)
-        if os.path.isdir(folder_path):
-            for subfolder_name in os.listdir(folder_path):
-                subfolder_path = os.path.join(folder_path, subfolder_name)
-                if os.path.isdir(subfolder_path):
-                    for image_name in os.listdir(subfolder_path):
-                        image_path = os.path.join(subfolder_path, image_name)
-                        if os.path.isfile(image_path):
-                            new_folder_name = os.path.splitext(image_name)[0]
-                            new_folder_path = os.path.join(folder_path, new_folder_name)
-                            os.makedirs(new_folder_path, exist_ok=True)
-                            new_image_path = os.path.join(new_folder_path, image_name)
-                            if image_path != new_image_path:
-                                shutil.copy2(image_path, new_image_path)
-                            # Удаляем исходный файл после копирования, если нужно
-                            os.remove(image_path)
-                    # Удаляем старую папку после копирования
-                    shutil.rmtree(subfolder_path)
+# Создадим новые 10 папок для отсортированных картинок и вставимк ним путь
+sorted_folder = 'путь/к/папке/с/отсортированными/картинками'
+os.makedirs(sorted_folder, exist_ok=True)
+sorted_folder = 'путь/к/папке/с/отсортированными/картинками'
+os.makedirs(sorted_folder, exist_ok=True)
+sorted_folder = 'путь/к/папке/с/отсортированными/картинками'
+os.makedirs(sorted_folder, exist_ok=True)
+sorted_folder = 'путь/к/папке/с/отсортированными/картинками'
+os.makedirs(sorted_folder, exist_ok=True)
+sorted_folder = 'путь/к/папке/с/отсортированными/картинками'
+os.makedirs(sorted_folder, exist_ok=True)
+sorted_folder = 'путь/к/папке/с/отсортированными/картинками'
+os.makedirs(sorted_folder, exist_ok=True)
+sorted_folder = 'путь/к/папке/с/отсортированными/картинками'
+os.makedirs(sorted_folder, exist_ok=True)
+sorted_folder = 'путь/к/папке/с/отсортированными/картинками'
+os.makedirs(sorted_folder, exist_ok=True)
+sorted_folder = 'путь/к/папке/с/отсортированными/картинками'
+os.makedirs(sorted_folder, exist_ok=True)
+sorted_folder = 'путь/к/папке/с/отсортированными/картинками'
+os.makedirs(sorted_folder, exist_ok=True)
+# Перебор папок 1, 2, 3
+for subfolder in ['1', '2', '3']:
+    subfolder_path = os.path.join(folder_A, subfolder)
 
-sort_images(root_folder_path)
+    # Перебор картинок в каждой папке
+    for filename in os.listdir(subfolder_path):
+        if filename.endswith('.jpg') or filename.endswith('.png'):
+            # Формирование нового имени файла с учетом порядка
+            new_filename = f'{subfolder}_{filename}'
+
+            # Путь к исходному файлу
+            src_path = os.path.join(subfolder_path, filename)
+
+            # Путь к новому файлу
+            dest_path = os.path.join(sorted_folder, new_filename)
+
+            # Копирование файла в новую папку с новым именем
+            shutil.copy(src_path, dest_path)
